ea7eb820ffbbd7c5201b0b4cc47eb84fd79db56f
katsuyatakano-33 <wbnhq4682@yahoo.co.jp>
2025-11-11 22:13:35 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+//btnがクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+ text.textContent = 'ボタンがクリックされました。';
+});
